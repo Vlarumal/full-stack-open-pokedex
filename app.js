af6b984fafb82d3f1f0888152babf1d3142706eb
Vlarumal <177475520+Vlarumal@users.noreply.github.com>
2025-04-22 19:33:07 +0300

    test: success notification

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('fail notification v4') // change this string to ensure a new version deployed
+  res.send('success notification') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
