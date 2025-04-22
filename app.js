0252524137258efc3447188b027f5ec21c1ea318
Vlarumal <177475520+Vlarumal@users.noreply.github.com>
2025-04-22 16:46:48 +0300

    fix: conditions #skip

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('fix: skip condition') // change this string to ensure a new version deployed
+  res.send('fix: conditions') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
